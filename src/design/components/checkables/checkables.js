c6a9fd38e1de452f07be0539971d61c6f9082585
Jo Colina <jo.colina@weezevent.com>
2020-11-17 08:36:38 +0100

    Checkables: add required star

--- src/design/components/checkables/checkables.js
+++ src/design/components/checkables/checkables.js
@@ -2,18 +2,20 @@ import React from 'react';
 
 import Style from './checkable.module.css';
 
-export function Checkable({ type='checkbox', label='', onChange=()=>{}, style={}, children, className='', name='', disabled, message, error, warning, ...rest }) {
+export function Checkable({ type='checkbox', label='', onChange=()=>{}, style={}, children, className='', name='', disabled, message, error, warning, required, ...rest }) {
 	let _type = type === 'toggle' ? 'checkbox' : type;
 
-	let classes = Object.entries({ error, disabled, warning }).filter(([k, v]) => v).map(([k]) => Style[k]).join(' ');
+	let classes = Object.entries({ error, disabled, warning, required }).filter(([k, v]) => v).map(([k]) => Style[k]).join(' ');
 	let _class = `${Style['checkable']} ${className} ${classes}`;
 
 	return (
 		<label style={style} type={type} className={_class}>
 			<input type={_type} name={name} onChange={onChange} className={type} disabled={disabled} {...rest}/>
-			<span className={`checkable-mark ${type}`}/>
+			<span className={`${Style['checkable-mark']} ${type}`}/>
 			<div className={Style['checkable-label-container']}>
-				{children || label}
+				<div className={Style['checkable-label']}>
+					{children || label}
+				</div>
 				{message && <em className={Style['message']}>{message}</em>}
 			</div>
 		</label>
