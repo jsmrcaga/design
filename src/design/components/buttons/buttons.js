4218c0ff87d419b232033ee16c9f1464eee0c429
Jo Colina <jo.colina@weezevent.com>
2021-01-09 22:20:47 +0100

    Tags: add pill + warning/success light colors

--- src/design/components/buttons/buttons.js
+++ src/design/components/buttons/buttons.js
@@ -2,8 +2,8 @@ import React from 'react';
 
 import Style from './buttons.module.css';
 
-export function Button({ big, primary, success, disabled, clear, danger, dark, small, flat, content, loading, children, className='', ...rest }) {
-	let _class = Object.entries({ big, primary, success, clear, danger, dark, small, flat }).filter(([k, v]) => v).map(([k]) => Style[k]).join(' ');
+export function Button({ big, primary, success, warning, disabled, clear, danger, dark, small, flat, content, loading, children, className='', ...rest }) {
+	let _class = Object.entries({ big, primary, success, warning, clear, danger, dark, small, flat }).filter(([k, v]) => v).map(([k]) => Style[k]).join(' ');
 	_class = `${_class} ${className}`;
 	return (
 		<button disabled={disabled || loading} className={`${Style['button']} ${_class}`} {...rest}>
@@ -16,11 +16,12 @@ export function Button({ big, primary, success, disabled, clear, danger, dark, s
 	);
 }
 
-export function Tag({ info ,primary, success, disabled, clear, danger, dark, small, flat, content, children, className='', clearable, onClear=()=>{}, ...rest }) {
-	let _class = Object.entries({ info, primary, success, clear, danger, dark, small, flat }).filter(([k, v]) => v).map(([k]) => Style[k]).join(' ');
+export function Tag({ info, pill=false, primary, warning, success, disabled, clear, danger, dark, small, flat, content, children, className='', clearable, onClear=()=>{}, ...rest }) {
+	let _class = Object.entries({ info, pill, primary, warning, success, clear, danger, dark, small, flat }).filter(([k, v]) => v).map(([k]) => Style[k]).join(' ');
 	_class = `${_class} ${className}`;
 	return (
 		<div disabled={disabled} className={`${Style['tag']} ${_class}`} {...rest}>
+			{pill && <div className={Style['pill']}/>}
 			{children || content}
 			{clearable &&
 				<i className={`gg-close-o ${Style['gg-icon']} ${Style['small']}`} onClick={onClear}/>
