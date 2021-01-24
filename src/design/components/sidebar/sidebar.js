13b66d9ff6e7be3ecd11439012a56675fc11dc5c
Jo Colina <jo.colina@weezevent.com>
2021-01-24 19:33:39 +0100

    Style: fixed drawer height & use useClickAway

--- src/design/components/sidebar/sidebar.js
+++ src/design/components/sidebar/sidebar.js
@@ -4,12 +4,22 @@ import { ReactComponent as UserSVG } from '../../iconography/user.svg';
 
 import Style from './sidebar.module.css';
 
-export function SidebarLayout({ Sidebar=null, show=false, children, className='' }) {
+import { useClickAway } from '../../../hooks/useClickAway';
+
+export function SidebarLayout({ Sidebar=null, show=false, children, onClickAway=()=>{}, className='' }) {
+	const sidebarRef = React.useRef(null);
 	const [ shouldShow, setShow ] = React.useState(show);
+
 	let sidecontent = Sidebar ? (React.isValidElement(Sidebar) ? Sidebar : <Sidebar/>) : Sidebar;
+
+	useClickAway(sidebarRef, () => {
+		setShow(false);
+		onClickAway();
+	});
+
 	return (
 		<div className={Style['sidebar-layout']}>
-			<div className={`${Style['sidebar-container']} ${shouldShow ? Style['displayed'] : ''} ${className}`}>
+			<div className={`${Style['sidebar-container']} ${shouldShow ? Style['displayed'] : ''} ${className}`} ref={sidebarRef}>
 				<UserSVG onClick={() => setShow(!shouldShow)} className={`${Style['menu-icon']} ${Style['sidebar']}`}/>
 				{sidecontent}
 			</div>
