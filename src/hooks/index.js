b040743799c4216adfd8e8ba7abc1c5b26f2ad7f
Jo Colina <jo.colina@weezevent.com>
2022-10-30 15:04:40 +0100

    Hooks: improve useFetch

--- src/hooks/index.js
+++ src/hooks/index.js
@@ -1,4 +1,5 @@
 export { useFetch } from './useFetch';
 export { useClickAway } from './useClickAway';
 export { useCopy } from './useCopy';
+export { useLocalStorageChange } from './useLocalStorageChange';
 export * from './useTime';
