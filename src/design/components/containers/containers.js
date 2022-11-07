56f63eaa65ff8b469eb96bb20beec3f3e2cc0fb8
Jo Colina <jo.colina@weezevent.com>
2022-11-07 22:33:10 +0100

    Containers: inline-block & forwardRef

--- src/design/components/containers/containers.js
+++ src/design/components/containers/containers.js
@@ -3,45 +3,45 @@ import { classnames } from '../../utils/classnames';
 
 import Style from './containers.module.css';
 
-export function Container({ className='', children, fluid=false, centered=false, ...rest }) {
+export const Container = React.forwardRef(({ className='', children, fluid=false, centered=false, ...rest }, ref) => {
 	let _class = Object.entries({ fluid, centered }).filter(([k, v]) => v).map(([k]) => Style[k]).join(' ');
 	return (
-		<div className={classnames(Style['container'], className, _class)} {...rest}>
-			{children}
-		</div>
-	);
-}
-
-
-export function Box({ className='', children, lifted, ...rest }) {
-	return (
-		<div className={classnames(Style.box, className, { [Style.lifted]: lifted })} {...rest}>
-			{children}
-		</div>
-	);
-}
-
-export function Card({ className='', children, ...rest }) {
-	return (
-		<div className={classnames(Style['card'], className)} {...rest}>
-			{children}
-		</div>
-	);
-}
-
-export const CardGroup = React.forwardRef(({ className='', children }, ref) => {
-	return (
-		<div className={classnames(className, Style['card-group'])}>
+		<div ref={ref} className={classnames(Style['container'], className, _class)} {...rest}>
 			{children}
 		</div>
 	);
 });
 
-export function Row({ className='', children, ...rest }) {
-	let _class = `${Style['row']} ${className}`;
+
+export const Box = React.forwardRef(({ className='', children, lifted, ...rest }, ref) => {
 	return (
-		<div className={_class} {...rest}>
+		<div ref={ref} className={classnames(Style.box, className, { [Style.lifted]: lifted })} {...rest}>
 			{children}
 		</div>
 	);
-}
+});
+
+export const Card = React.forwardRef(({ className='', children, ...rest }, ref) => {
+	return (
+		<div ref={ref} className={classnames(Style['card'], className)} {...rest}>
+			{children}
+		</div>
+	);
+});
+
+export const CardGroup = React.forwardRef(({ className='', children }, ref) => {
+	return (
+		<div ref={ref} className={classnames(className, Style['card-group'])}>
+			{children}
+		</div>
+	);
+});
+
+export const Row = React.forwardRef(({ className='', children, ...rest }, ref) => {
+	let _class = `${Style['row']} ${className}`;
+	return (
+		<div ref={ref} className={_class} {...rest}>
+			{children}
+		</div>
+	);
+});
